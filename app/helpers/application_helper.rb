9cb04318ccd1e099ae6804285c024d8ac63c872a
Andy Schmitz <andy.schmitz@gmail.com>
2017-08-14 17:46:57 -0500

    fix: actually handle missing artists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def artists_to_text(artists)
-    '[unknown]' unless artists.respond_to?(:map)
+    return '[unknown]' unless artists
     
     artists.map do |artist|
       if artist['uri']
