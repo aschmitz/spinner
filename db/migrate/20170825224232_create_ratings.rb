1544c3bf537de74d032542d780e9a3553e5effa0
aschmitz <andy.schmitz@gmail.com>
2017-08-25 20:09:10 -0500

    fix: thanks, magic Rails indices!
    No idea why this didn't cause problems in sqlite, but here we are.

--- db/migrate/20170825224232_create_ratings.rb
+++ db/migrate/20170825224232_create_ratings.rb
@@ -5,8 +5,6 @@ class CreateRatings < ActiveRecord::Migration[5.1]
       t.belongs_to :user
       t.integer :score
       t.timestamps
-      t.index(:track_id)
-      t.index(:user_id)
       t.index([:track_id, :user_id], unique: true)
     end
   end
