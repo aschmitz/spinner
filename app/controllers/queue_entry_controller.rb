76e878ab9f5343b91ef648f293dd4ccf5cffc1fc
aschmitz <aschmitz@nccgroup.com>
2021-09-22 11:07:37 -0500

    fix: album lookups
    Also cleans up some formatting, removes GMusic fallbacks since it's gone
    now.

--- app/controllers/queue_entry_controller.rb
+++ app/controllers/queue_entry_controller.rb
@@ -1,14 +1,14 @@
 class QueueEntryController < ApplicationController
   before_action :load_and_authorize, only: [:destroy, :show, :update]
-  
+
   def index
     @queue_entries = QueueEntry.where(user: current_user)
   end
-  
+
   def destroy
     @queue_entry.destroy
   end
-  
+
   def create
     if (uri = params[:uri])
       #Allow for uri[] syntax in the query string to queue a list of tracks
@@ -26,7 +26,8 @@ class QueueEntryController < ApplicationController
       end
       render plain: 'ok'
     elsif (album_uri = params[:album_uri])
-      tracks = MopidyClient.instance.invoke('core.library.lookup', {:uris => [album_uri]})
+      tracks = MopidyClient.instance.invoke('core.library.lookup', { uris: [album_uri] })
+      tracks = tracks.values.first
       tracks.each do |track_tl|
         track = Track.from_uri(track_tl['uri'])
         queue_track(track)
@@ -36,20 +37,20 @@ class QueueEntryController < ApplicationController
       render plain: "Couldn't figure out what you intended."
     end
   end
-  
+
   private
-  
+
   def load_and_authorize
     @queue_entry = QueueEntry.find(params[:id])
     raise 'No such queue entry' unless @queue_entry
-    
+
     raise 'Not your queue entry' unless @queue_entry.user == current_user
   end
-  
+
   def queue_track(track)
     # Add this to the user's library if it didn't already exist there
     LibraryTrack.where(user: current_user, track: track).first_or_create
-    
+
     # Add this to the user's queue
     QueueEntry.create(user: current_user, track: track)
   end
